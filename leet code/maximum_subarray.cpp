e784252265baa4d54e83146520d238d1660785da
jharkawat <jalaj.iitkgp@gmail.com>
2020-04-03 15:35:46 +0530

    leet code day 3 submission

---
+++ leet code/maximum_subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int max = nums[0];
+        int sum = 0;
+        for(int i = 0; i< nums.size() ; i++)
+        {
+            sum += nums[i];
+            if(sum > max)
+            {
+                max = sum;
+            }
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return max;        
+    }
+};
