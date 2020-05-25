a4f10cd922547a18212fcc746f47fe26a90c22e9
jharkawat <jalaj.iitkgp@gmail.com>
2020-05-25 13:02:08 +0530

    final add

---
+++ practice/cf_24_05_2020/q5.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define test() int t;cin>>t;while(t--)
+#define For(_i , N) for(int _i = 0 ;_i < N ; _i ++)
+#define ForR(_i , N) for(int _i = N ;_i > 0 ; _i --)
+#define ForI(_i , a , N) for(int _i = a ;_i < N ; _i ++)
+#define ForIR(_i , a , N) for(int _i = N ;_i > a ; _i --)
+#define INF (2139062143)
+
+typedef vector< int > vi;
+typedef vector< vi > vvi;
+typedef pair< int,int > ii;
+typedef pair< long long int ,long long int > iil;
+ 
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for( auto i = (c).begin(); i != (c).end(); i++)
+ 
+#define edl '\n'
+#define ll long long int
+#define ff first
+#define ss second
+
+int main()
+{
+    int t;
+    cin >> t;
+    For(i,t)
+    {
+        int n;
+        cin >> n;
+        vector<vector<int >> arr;
+        For(j,n)
+        {
+            vector<int> push;
+            string t;
+            cin >> t;
+            For(k,n)
+            {
+                char ch = t[k];
+                int m = ch -'0';
+                push.pb(m);
+            }
+            arr.pb(push);
+        }
+        int stag = 0; 
+        For(j,(n-1))
+        {
+            For(k,(n-1))
+            {
+                if(arr[j][k] == 1)
+                {
+                    if((arr[j+1][k] == 0) && (arr[j][k+1] == 0))
+                    {
+                        //cout << "no" << endl;
+                        stag = 1;
+                    }
+                }
+            }
+        }
+        if(stag == 0)
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+        
+
+    }
+}
